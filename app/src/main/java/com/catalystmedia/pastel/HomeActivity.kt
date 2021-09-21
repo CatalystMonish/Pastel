0bb5fe50ebd47e32fc65a3341146d86d25fac351
Vaama Nikam <vaamanikam11@gmail.com>
2021-09-21 08:15:35 +0530

    Vaama Second Commit

--- app/src/main/java/com/catalystmedia/pastel/HomeActivity.kt
+++ app/src/main/java/com/catalystmedia/pastel/HomeActivity.kt
@@ -2,10 +2,41 @@ package com.catalystmedia.pastel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.catalystmedia.pastel.fragments.ActivityFragment
+import com.catalystmedia.pastel.fragments.HealthFragment
+import com.catalystmedia.pastel.fragments.HomeFragment
+import com.catalystmedia.pastel.fragments.ProfileFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val firstFragment= ActivityFragment()
+        val secondFragment=HealthFragment()
+        val thirdFragment=HomeFragment()
+        val fourthFragment=ProfileFragment()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.activity->setCurrentFragment(firstFragment)
+                R.id.health->setCurrentFragment(secondFragment)
+                R.id.home->setCurrentFragment(thirdFragment)
+                R.id.profile->setCurrentFragment(fourthFragment)
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
---
+++ app/src/main/java/com/catalystmedia/pastel/fragments/ActivityFragment.kt
@@ -0,0 +1,29 @@
+package com.catalystmedia.pastel.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.catalystmedia.pastel.R
+
+// TODO: Rename parameter arguments, choose names that match
+
+class ActivityFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activity, container, false)
+    }
+
+
+}
